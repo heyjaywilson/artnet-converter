98762d08bfcad02c0ce0bfca7faed043592d781d
maeganwilson_ <maegan@maeganwilson.com>
2019-02-15 10:35:55 -0600

    Renamed

--- artnet-app/Views/FixtureNoteViewController.swift
+++ artnet-app/Views/FixtureNoteViewController.swift
@@ -6,24 +6,24 @@
 //  Copyright © 2018 MW. All rights reserved.
 //
 //
-//import UIKit
+import UIKit
 //
-//class FixtureNotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-//    @IBOutlet weak var fixtureNoteTableView: UITableView!
+class FixtureNoteViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var fixtureNoteTableView: UITableView!
 //    @IBOutlet weak var fixtureUni: UILabel!
 //
 //    // var calc = Calc(universe: "1", artsub: "0", artuni: "1", note: [])
 //    var index = 0
 //
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 //        fixtureUni.text = "\(calc.universe)"
 //
-//        fixtureNoteTableView.delegate = self
-//        fixtureNoteTableView.dataSource = self
+        fixtureNoteTableView.delegate = self
+        fixtureNoteTableView.dataSource = self
 //
 //        fixtureNoteTableView.tableFooterView = UIView(frame: CGRect.zero)
-//    }
+    }
 //
 //    @IBAction func addNote(_ sender: Any) {
 //        let alertController = UIAlertController(title: "Add new note", message: "", preferredStyle: .alert)
@@ -47,11 +47,12 @@
 //        self.present(alertController, animated: true, completion: nil)
 //    }
 //
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
 //        return calc.note.count
-//    }
+    }
 //
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
 //        let row = indexPath.row
 //
@@ -60,7 +61,7 @@
 //        cell.textLabel?.text = calc.note[row]
 //
 //        return cell
-//    }
+    }
 //
 //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
 //        if editingStyle == .delete {
@@ -79,4 +80,4 @@
 //        // Pass the selected object to the new view controller.
 //    }
 //    */
-//}
+}
