d1b05f1bc8157ad6d468e88b6773add464aebc00
maeganwilson_ <maegan@maeganwilson.com>
2019-08-31 22:55:07 -0500

    Working on #17 and the ability to read notes.

---
+++ uni-converter/uni-converter/Views/CalculationViews/NotesView.swift
@@ -0,0 +1,21 @@
+//
+//  NotesView.swift
+//  uni-converter
+//
+//  Created by Maegan Wilson on 8/31/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesView()
+    }
+}
