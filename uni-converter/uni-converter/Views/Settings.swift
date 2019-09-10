700ae130f1d293cfb01cbe4deadce49020c40cd1
maeganwilson_ <maegan@maeganwilson.com>
2019-09-09 22:20:29 -0400

    Working on adding links

--- uni-converter/uni-converter/Views/Settings.swift
+++ uni-converter/uni-converter/Views/Settings.swift
@@ -12,15 +12,38 @@ struct Settings: View {
     @EnvironmentObject var settingsManager: SettingsManager
     
     var body: some View {
-        Form {
-            Section(header: Text("Universe Settings")){
-                Toggle(isOn: $settingsManager.zeroUni) {
-                    Text("Primary Universes is \(settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))")
+        NavigationView{
+            List {
+                Section(header: Text("Universe Settings").padding(.top)){
+                    Toggle(isOn: $settingsManager.zeroUni) {
+                        Text("Primary Universes is \(settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))")
+                    }
+                    Toggle(isOn: $settingsManager.zeroArt) {
+                        Text("Console needs ArtNet settings as \(settingsManager.returnZeroOrOne(self.settingsManager.zeroArt)) based")
+                    }
                 }
-                Toggle(isOn: $settingsManager.zeroArt) {
-                    Text("Console needs ArtNet settings as \(settingsManager.returnZeroOrOne(self.settingsManager.zeroArt)) based")
+                Section(header: Text("About").padding(.top)){
+                    NavigationLink(destination: Text("About")) {
+                        Text("About the App")
+                    }
+                    NavigationLink(destination: Text("Tipping")) {
+                        VStack(alignment: .leading) {
+                            Text("Enjoying the app?")
+                            Text("Tap here to add to the tip jar")
+                                .font(.subheadline)
+                        }
+                    }
                 }
-            }
+                Section(header: Text("Support").padding(.top)){
+                    Text("🐛 Bugs (🔗 to GitHub)").onTapGesture {
+                        if let url = URL(string: "https://www.hackingwithswift.com") {
+                            UIApplication.shared.open(url)
+                        }
+                    }
+                    Text("⚒️ Requests (🔗 to GitHub)")
+                    Text("✉️ Contact (🔗 to Email)")
+                }
+            }.navigationBarTitle("Settings & Info")
         }
     }
 }
