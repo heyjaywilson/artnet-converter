e919180dc46c350a4350d79101f3398ba8539043
Maegan Wilson <maegan@maeganwilson.com>
2020-01-20 07:13:38 -0600

    HEX conversion is working

--- uni-converter/uni-converter/Views/CalculateView.swift
+++ uni-converter/uni-converter/Views/CalculateView.swift
@@ -5,22 +5,29 @@
 //  Created by Maegan Wilson on 8/29/19.
 //  Copyright © 2019 MW. All rights reserved.
 //
-
+import Foundation
 import SwiftUI
 
 struct CalculateView: View {
   @EnvironmentObject var calcManager: CalculationManager
   @EnvironmentObject var settingsManager: SettingsManager
   
+  @State private var isHexShown = false
   var body: some View {
     VStack {
       HStack {
         UniverseView(uni: calcManager.prim_uni)
         Spacer()
         Group{
-          SubView(sub: calcManager.subnet)
-            .padding(.trailing)
-          UniverseView(uni: calcManager.art_uni)
+          if isHexShown {
+            Text(showHex())
+          } else {
+            SubView(sub: calcManager.subnet)
+              .padding(.trailing)
+            UniverseView(uni: calcManager.art_uni)
+          }
+        }.onTapGesture {
+          self.isHexShown.toggle()
         }
       }
       .padding(.horizontal)
@@ -34,5 +41,11 @@ struct CalculateView: View {
         self.calcManager.setNum(primUni: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))
       }
     }
+  }
+  
+  func showHex() -> String{
+    let st = String(format:"%02X", calcManager.prim_uni)
+    print(st)
+    return st
   }
 }
