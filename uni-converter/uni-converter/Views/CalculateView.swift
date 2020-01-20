1e756a4aa6beb19f5eabf2b2c1a8ad1ce237e4b4
Maegan Wilson <maegan@maeganwilson.com>
2020-01-20 06:52:24 -0600

    Working on hex

--- uni-converter/uni-converter/Views/CalculateView.swift
+++ uni-converter/uni-converter/Views/CalculateView.swift
@@ -9,28 +9,30 @@
 import SwiftUI
 
 struct CalculateView: View {
-    @EnvironmentObject var calcManager: CalculationManager
-    @EnvironmentObject var settingsManager: SettingsManager
-    
-    var body: some View {
-        VStack {
-            HStack {
-                UniverseView(uni: calcManager.prim_uni)
-                Spacer()
-                SubView(sub: calcManager.subnet)
-                    .padding(.trailing)
-                UniverseView(uni: calcManager.art_uni)
-            }
-            .padding(.horizontal)
-            .padding(.top, 50)
-            Spacer()
-            AllButtons()
-            .padding(.bottom, 50)
-        }.onAppear{
-            if self.calcManager.prim_uni <= 1 {
-                self.calcManager.setNum(art: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroArt))
-                self.calcManager.setNum(primUni: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))
-            }
+  @EnvironmentObject var calcManager: CalculationManager
+  @EnvironmentObject var settingsManager: SettingsManager
+  
+  var body: some View {
+    VStack {
+      HStack {
+        UniverseView(uni: calcManager.prim_uni)
+        Spacer()
+        Group{
+          SubView(sub: calcManager.subnet)
+            .padding(.trailing)
+          UniverseView(uni: calcManager.art_uni)
         }
+      }
+      .padding(.horizontal)
+      .padding(.top, 50)
+      Spacer()
+      AllButtons()
+        .padding(.bottom, 50)
+    }.onAppear{
+      if self.calcManager.prim_uni <= 1 {
+        self.calcManager.setNum(art: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroArt))
+        self.calcManager.setNum(primUni: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))
+      }
     }
+  }
 }
