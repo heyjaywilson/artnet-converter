2de156944825bd4337c25996d9495c169b58928c
Maegan Wilson <maegan@maeganwilson.com>
2020-01-21 20:12:06 -0600

    Working on hex values

--- uni-converter/uni-converter/Views/Answers/PrimUniView.swift
+++ uni-converter/uni-converter/Views/Answers/PrimUniView.swift
@@ -9,19 +9,36 @@
 import SwiftUI
 
 struct UniverseView: View {
-    var uni: Int
-    var body: some View {
-        VStack {
-            Text("universe")
-                .font(.subheadline)
-            Text("\(uni)")
-                .font(.title)
-        }
+  var isPrimeUni = false
+  var uni: Int
+  var body: some View {
+    VStack {
+      Text("universe")
+        .font(.subheadline)
+      Text(determineOutput())
+        .font(.title)
     }
+  }
+  
+  func determineOutput() -> String{
+    var output = ""
+    if isPrimeUni {
+      output = "\(uni)"
+    } else {
+      // check userdefault for hex
+      // if true output = showHex()
+    }
+    return output
+  }
+  
+  func showHex() -> String{
+    let hexValue = String(format:"%02X", uni)
+    return hexValue
+  }
 }
 
 struct UniverseView_Previews: PreviewProvider {
-    static var previews: some View {
-        UniverseView(uni: 8)
-    }
+  static var previews: some View {
+    UniverseView(uni: 8)
+  }
 }
--- uni-converter/uni-converter/Views/CalculateView.swift
+++ uni-converter/uni-converter/Views/CalculateView.swift
@@ -12,40 +12,27 @@ struct CalculateView: View {
   @EnvironmentObject var calcManager: CalculationManager
   @EnvironmentObject var settingsManager: SettingsManager
   
-  @State private var isHexShown = false
   var body: some View {
     VStack {
       HStack {
-        UniverseView(uni: calcManager.prim_uni)
+        UniverseView(isPrimeUni: true, uni: calcManager.prim_uni)
         Spacer()
         Group{
-          if isHexShown {
-            Text(showHex())
-          } else {
-            SubView(sub: calcManager.subnet)
-              .padding(.trailing)
-            UniverseView(uni: calcManager.art_uni)
-          }
-        }.onTapGesture {
-          self.isHexShown.toggle()
+          SubView(sub: calcManager.subnet)
+            .padding(.trailing)
+          UniverseView(uni: calcManager.art_uni)
+        }
+        .padding(.horizontal)
+        .padding(.top, 50)
+        Spacer()
+        AllButtons()
+          .padding(.bottom, 50)
+      }.onAppear{
+        if self.calcManager.prim_uni <= 1 {
+          self.calcManager.setNum(art: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroArt))
+          self.calcManager.setNum(primUni: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))
         }
       }
-      .padding(.horizontal)
-      .padding(.top, 50)
-      Spacer()
-      AllButtons()
-        .padding(.bottom, 50)
-    }.onAppear{
-      if self.calcManager.prim_uni <= 1 {
-        self.calcManager.setNum(art: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroArt))
-        self.calcManager.setNum(primUni: self.settingsManager.returnZeroOrOne(self.settingsManager.zeroUni))
-      }
     }
-  }
-  
-  func showHex() -> String{
-    let st = String(format:"%02X", calcManager.prim_uni)
-    print(st)
-    return st
   }
 }
