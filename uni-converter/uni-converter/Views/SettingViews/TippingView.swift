581652d30839d6cf53d05d2e44d3b11d315b0eeb
Maegan Wilson <maegan@maeganwilson.com>
2020-04-29 11:34:12 -0500

    Changed formatting

--- uni-converter/uni-converter/Views/SettingViews/TippingView.swift
+++ uni-converter/uni-converter/Views/SettingViews/TippingView.swift
@@ -9,48 +9,45 @@
 import SwiftUI
 import StoreKit
 struct TippingView: View {
-    @State private var products: [SKProduct] = []
-    @State private var totalTipped: Double = 0.0
-    var store = ArtnetAppProducts.store
+  @State private var products: [SKProduct] = []
+  @State private var totalTipped: Double = 0.0
+  
+  var store = ArtnetAppProducts.store
+  var showUpdate = false
+  
+  static let priceFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
     
-    static let priceFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        
-        formatter.formatterBehavior = .behavior10_4
-        formatter.numberStyle = .currency
-        
-        return formatter
-    }()
+    formatter.formatterBehavior = .behavior10_4
+    formatter.numberStyle = .currency
     
-    var body: some View {
-        VStack{
-            Text("If you enjoy using the app, please consider giving Maegan a tip using an in-app purchase. Your support is greatly appreciated.")
-            List{
-                HStack{
-                    Text("Total Tipped")
-                    Spacer()
-                    Text("")
-                }
-                ForEach(self.products, id: \.productIdentifier){ product in
-                    TippingRow(product: product)
-                }
-            }
+    return formatter
+  }()
+  
+  var body: some View {
+    VStack{
+      Text("If you enjoy using the app, please consider giving Maegan a tip using an in-app purchase. Your support is greatly appreciated.")
+      List{
+        ForEach(self.products, id: \.productIdentifier){ product in
+          TippingRow(product: product)
         }
-        .padding(.horizontal)
-        .navigationBarTitle("👍 Tips")
-        .onAppear{
-            self.store.requestProducts { (success, products) in
-                if success {
-                    self.products = products!
-                }
-            }
-            
-        }
+      }
     }
+    .padding(.horizontal)
+    .navigationBarTitle("👍 Tips")
+    .onAppear{
+      self.store.requestProducts { (success, products) in
+        if success {
+          self.products = products!
+        }
+      }
+      
+    }
+  }
 }
 
 struct TippingView_Previews: PreviewProvider {
-    static var previews: some View {
-        TippingView()
-    }
+  static var previews: some View {
+    TippingView()
+  }
 }
